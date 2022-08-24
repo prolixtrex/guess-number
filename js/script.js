0d649ed835cf923f5acfee3be876c30870c19eea
prolixtrex <prolixtrex@users.noreply.github.com>
2022-08-24 23:46:41 +0100

    Accept only number digits

--- js/script.js
+++ js/script.js
@@ -10,10 +10,14 @@ function random(event) {
 	if (!guessNum) {
 		alert("Input some digits");
 		return false;
+	} else if (/^[0-9]+$/.test(guessNum) === false) {
+		alert("Invalid Input");
 	} else if (guessNum.length > 3) {
 		alert("Digits can not be more than 3");
 	} else {
 		let numbers = [0,1,2,3,4,5,6,7,8,9];
+
+		//shuffles the number arrays
 		for (var i = numbers.length - 1; i > 0; i--) {
 			var j = Math.floor(Math.random() * (i + 1));
 			var temp = numbers[i];
@@ -21,21 +25,24 @@ function random(event) {
 			numbers[j] = temp;
 		}
 
+		//cuts the first 4 part of the shuffled arrays
 		let generated = numbers.slice(0,4,).join('');
 
 		let arr = [];
 		let arr2 = [];
 		let arr3 =[];
 		
+		//converts generated number to array
 		for (const item of generated) {
 			arr.push(item);
 		}
 
+		//converts input number to arrey
 		for (const num of guessNum) {
 			arr2.push(num);
 		}
 
-
+		//loops through the arrays and search for input digits inside the generated numbers
 		for (const items in arr) {
 			for (const num in arr2) {
 				if (arr[items] == arr2[num]) {
@@ -43,7 +50,9 @@ function random(event) {
 				}
 			}
 		}
-		let arr4 = [...new Set(arr3)];
+
+		let arr4 = [...new Set(arr3)]; //prevent duplicate occurance of numbers in the arrays
+		
 		randomNum.innerHTML = generated;
 		matched.innerHTML = arr4;
 		correct.innerHTML = arr4.length;
